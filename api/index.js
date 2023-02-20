98702005f3d749010c32ed07eb380574afc463b2
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-02-20 12:23:23 -0300

    chores: vercel fix

--- api/index.js
+++ api/index.js
@@ -8,6 +8,12 @@ const app = express();
 function main() {
     db();
     app.use(cors());
+
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        next();
+      });
+
     app.use(express.json({limit : 52428800, extended : true}));
     app.use(express.urlencoded({limit : 52428800, extended : true}));
     app.use(fileupload({useTempFiles: true}));
