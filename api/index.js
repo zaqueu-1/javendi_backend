050701677d1b983957ed647f179e6f32e448a6d8
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-02-20 12:26:13 -0300

    chores: remove cors

--- api/index.js
+++ api/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const fileupload = require('express-fileupload');
-const cors = require('cors');
 const db = require('../db/db');
 const routes = require('../routes/router');
 const app = express();
 
 function main() {
     db();
-    app.use(cors());
 
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
