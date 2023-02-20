813a679ed80719def2031f45900a79ffb4cd7779
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-02-20 12:35:09 -0300

    chores: headers adjust

--- api/index.js
+++ api/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const fileupload = require('express-fileupload');
+const cors = require('cors');
 const db = require('../db/db');
 const routes = require('../routes/router');
 const app = express();
 
 function main() {
     db();
-
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
-        next();
-      });
-
+    app.use(cors());
     app.use(express.json({limit : 52428800, extended : true}));
     app.use(express.urlencoded({limit : 52428800, extended : true}));
     app.use(fileupload({useTempFiles: true}));
