c5cb76359300f8d3645be3f44dcef398e0423f4c
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-02-19 23:41:32 -0300

    chores: cors adjust

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const fileupload = require('express-fileupload')
-const cors = require('cors')
-const db = require('../db/db')
-const routes = require('../routes/router')
-const app = express()
+const fileupload = require('express-fileupload');
+const cors = require('cors');
+const db = require('../db/db');
+const routes = require('../routes/router');
+const app = express();
 
 function main() {
-    db()
-    app.use(express.json({limit : 52428800, extended : true}))
-    app.use(express.urlencoded({limit : 52428800, extended : true}))
-    app.use(cors())
-    app.use(fileupload({useTempFiles: true}))
-    app.use('/api', routes)
+    app.use(cors());
+    db();
+    app.use(express.json({limit : 52428800, extended : true}));
+    app.use(express.urlencoded({limit : 52428800, extended : true}));
+    app.use(fileupload({useTempFiles: true}));
+    app.use('/api', routes);
 
     app.listen(9001, function() {
         console.log('🪓')
