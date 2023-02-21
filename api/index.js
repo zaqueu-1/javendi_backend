46388db824e44e7caaa71b0566b55dd417ecd47a
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2023-02-21 07:07:30 -0300

    chores: cors config

--- api/index.js
+++ api/index.js
@@ -8,6 +8,14 @@ const app = express();
 function main() {
     db();
     app.use(cors());
+
+    app.options('*', cors({
+        allowedHeaders: 'Content-Type,Authorization',
+        methods: 'GET,POST,PUT,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204
+    }));
+    
     app.use(express.json({limit : 52428800, extended : true}));
     app.use(express.urlencoded({limit : 52428800, extended : true}));
     app.use(fileupload({useTempFiles: true}));
